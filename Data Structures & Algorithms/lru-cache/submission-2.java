3e171ee43e1ae3e22251a64fe8207e33ceba4f44
Paul_D_Edward <edward2000ed@gmail.com>
2026-06-11 07:58:11 +0530

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.java
@@ -0,0 +1,68 @@
+public class Node {
+    int key;
+    int val;
+    Node prev = null;
+    Node next = null;
+
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+    }
+}
+
+class LRUCache {
+    int cap;
+    HashMap<Integer, Node> cache;
+    Node head;
+    Node tail;
+
+    public LRUCache(int capacity) {
+        cap = capacity;
+        cache = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev= head;
+    }
+
+    private void remove(Node node) {
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void insert(Node node) {
+        Node prev = tail.prev;
+        prev.next = node;
+        node.prev = prev;
+        node.next = tail;
+        tail.prev = node;
+    }
+    
+    public int get(int key) {
+        if(cache.containsKey(key)) {
+            Node node = cache.get(key);
+            remove(node);
+            insert(node);
+            return node.val;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if (cache.containsKey(key)) {
+            remove(cache.get(key));
+        }
+
+        Node node = new Node(key, value);
+        cache.put(key, node);
+        insert(node);
+         
+        if(cache.size() > cap) {
+            Node lru = head.next;
+            remove(lru);
+            cache.remove(lru.key);
+        }
+    }
+}
